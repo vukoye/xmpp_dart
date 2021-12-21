1c4dfb73955f2cc0aa21ac69149e85e2d7422c7d
Sean Nuon <sean@slash.co>
2021-12-21 19:09:55 +0700

    wip: add message delivery

--- lib/xmpp_stone.dart
+++ lib/xmpp_stone.dart
@@ -24,3 +24,4 @@ export 'src/elements/stanzas/AbstractStanza.dart';
 export 'src/elements/XmppElement.dart';
 export 'src/features/message_archive/MessageArchiveManager.dart';
 export 'src/extensions/multi_user_chat/MultiUserChatManager.dart';
+export 'src/extensions/message_delivery/MessageDeliveryManager.dart';
