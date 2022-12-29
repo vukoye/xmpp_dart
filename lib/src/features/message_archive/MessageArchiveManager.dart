02c3a0d9733aae1fe2f7c24d739c8cdd9b10c83f
Hao-Cher Hong <rax333j@gmail.com>
2022-12-29 23:34:38 +0800

    add Future based query support

--- lib/src/features/message_archive/MessageArchiveManager.dart
+++ lib/src/features/message_archive/MessageArchiveManager.dart
@@ -1,11 +1,10 @@
+import 'dart:async';
+
 import 'package:xmpp_stone/src/elements/XmppAttribute.dart';
-import 'package:xmpp_stone/src/elements/XmppElement.dart';
 import 'package:xmpp_stone/src/elements/forms/QueryElement.dart';
 import 'package:xmpp_stone/src/elements/forms/XElement.dart';
 import 'package:xmpp_stone/src/features/servicediscovery/MAMNegotiator.dart';
-import '../../Connection.dart';
-import '../../data/Jid.dart';
-import '../../elements/stanzas/AbstractStanza.dart';
+import 'package:xmpp_stone/xmpp_stone.dart';
 import '../../elements/stanzas/IqStanza.dart';
 import '../../elements/forms/FieldElement.dart';
 
@@ -30,26 +29,92 @@ class MessageArchiveManager {
 
   bool? get hasExtended => MAMNegotiator.getInstance(_connection).hasExtended;
 
-  bool get isQueryByDateSupported => MAMNegotiator.getInstance(_connection).isQueryByDateSupported;
+  bool get isQueryByDateSupported =>
+      MAMNegotiator.getInstance(_connection).isQueryByDateSupported;
 
-  bool get isQueryByIdSupported => MAMNegotiator.getInstance(_connection).isQueryByIdSupported;
+  bool get isQueryByIdSupported =>
+      MAMNegotiator.getInstance(_connection).isQueryByIdSupported;
 
-  bool get isQueryByJidSupported => MAMNegotiator.getInstance(_connection).isQueryByJidSupported;
+  bool get isQueryByJidSupported =>
+      MAMNegotiator.getInstance(_connection).isQueryByJidSupported;
 
   MessageArchiveManager(this._connection);
 
-  void queryAll() {
+  Future<QueryResult> _queryMessages(
+      IqStanza iqStanza, QueryElement query) async {
+    final messages = <Message>[];
+    final completer = Completer<QueryResult>();
+
+    late StreamSubscription dataSubscription;
+    dataSubscription = _connection.inStanzasStream
+        .where((event) => event is MessageStanza)
+        .map((event) => Message.fromStanza(event as MessageStanza))
+        .where((event) => event.queryId == query.queryId)
+        .listen(
+      (event) {
+        messages.add(event);
+      },
+      onError: (Object error, StackTrace stackTrace) {
+        print(error);
+        print(stackTrace);
+      },
+    );
+
+    late StreamSubscription resultSubscription;
+    resultSubscription = _connection.inStanzasStream
+        .where((event) =>
+            event is IqStanza &&
+            event.type == IqStanzaType.RESULT &&
+            event.id == iqStanza.id)
+        .listen(
+      (event) {
+        dataSubscription.cancel();
+        resultSubscription.cancel();
+
+        try {
+          final iqStanza = event as IqStanza;
+          final finElement = iqStanza.getChild('fin')!;
+          final setElement = finElement.getChild('set')!;
+
+          completer.complete(QueryResult(
+            finElement.getAttribute('complete')!.value == 'true',
+            int.parse(setElement.getChild('count')!.textValue!),
+            setElement.getChild('first')?.textValue,
+            setElement.getChild('last')?.textValue,
+            messages,
+          ));
+        } catch (e) {
+          completer.completeError(Exception('Unable to parse result: ${event.buildXmlString()}'));
+        }
+        
+      },
+      onError: (Object error, StackTrace stackTrace) {
+        completer.completeError(error, stackTrace);
+      },
+    );
+
+    _connection.writeStanza(iqStanza);
+
+    return completer.future;
+  }
+
+  Future<QueryResult> queryAll() {
     var iqStanza = IqStanza(AbstractStanza.getRandomId(), IqStanzaType.SET);
     var query = QueryElement();
     query.setXmlns('urn:xmpp:mam:2');
     query.setQueryId(AbstractStanza.getRandomId());
     iqStanza.addChild(query);
-    _connection.writeStanza(iqStanza);
+    return _queryMessages(iqStanza, query);
   }
 
-  void queryByTime({DateTime? start, DateTime? end, Jid? jid, int? max}) {
+  Future<QueryResult> queryByTime(
+      {DateTime? start,
+      DateTime? end,
+      Jid? jid,
+      int? max,
+      bool flipPage = false}) {
     if (start == null && end == null && jid == null) {
-      queryAll();
+      return queryAll();
     } else {
       var iqStanza = IqStanza(AbstractStanza.getRandomId(), IqStanzaType.SET);
       var query = QueryElement();
@@ -72,25 +137,28 @@ class MessageArchiveManager {
         x.addField(FieldElement.build(varAttr: 'end', value: endStr));
       }
       if (jid != null) {
-        x.addField(FieldElement.build(varAttr: 'with', value: jid.userAtDomain));
+        x.addField(
+            FieldElement.build(varAttr: 'with', value: jid.userAtDomain));
       }
       if (max != null) {
-        query.addChild(
-          XmppElement('set')
+        query.addChild(XmppElement('set')
           ..addAttribute(
-            XmppAttribute('xmlns', 'http://jabber.org/protocol/rsm'))
-              ..addChild(XmppElement('max')
-              ..textValue = max.toString()
-            )
+              XmppAttribute('xmlns', 'http://jabber.org/protocol/rsm'))
+          ..addChild(XmppElement('max')..textValue = max.toString()));
+      }
+      if (flipPage) {
+        query.addChild(
+          XmppElement('flip-page'),
         );
       }
-      _connection.writeStanza(iqStanza);
+      return _queryMessages(iqStanza, query);
     }
   }
 
-  void queryById({String? beforeId, String? afterId, Jid? jid}) {
+  Future<QueryResult> queryById(
+      {String? beforeId, String? afterId, Jid? jid}) {
     if (beforeId == null && afterId == null && jid == null) {
-      queryAll();
+      return queryAll();
     } else {
       var iqStanza = IqStanza(AbstractStanza.getRandomId(), IqStanzaType.SET);
       var query = QueryElement();
@@ -103,16 +171,59 @@ class MessageArchiveManager {
       x.addField(FieldElement.build(
           varAttr: 'FORM_TYPE', typeAttr: 'hidden', value: 'urn:xmpp:mam:2'));
       if (beforeId != null) {
-        x.addField(FieldElement.build(varAttr: 'beforeId', value: beforeId));
+        x.addField(FieldElement.build(varAttr: 'before-id', value: beforeId));
       }
       if (afterId != null) {
-        x.addField(FieldElement.build(varAttr: 'afterId', value: afterId));
+        x.addField(FieldElement.build(varAttr: 'after-id', value: afterId));
       }
       if (jid != null) {
-        x.addField(FieldElement.build(varAttr: 'with', value: jid.userAtDomain));
+        x.addField(
+            FieldElement.build(varAttr: 'with', value: jid.userAtDomain));
       }
-      _connection.writeStanza(iqStanza);
+      return _queryMessages(iqStanza, query);
     }
+  }
+
+  Future<QueryResult> query(
+      {Jid? jid,
+      int? max,
+      String? after,
+      String? before,
+      bool flipPage = false}) {
+    var iqStanza = IqStanza(AbstractStanza.getRandomId(), IqStanzaType.SET);
+    var query = QueryElement();
+    query.setXmlns('urn:xmpp:mam:2');
+    query.setQueryId(AbstractStanza.getRandomId());
+    iqStanza.addChild(query);
+    var x = XElement.build();
+    x.setType(FormType.SUBMIT);
+    query.addChild(x);
+    x.addField(FieldElement.build(
+        varAttr: 'FORM_TYPE', typeAttr: 'hidden', value: 'urn:xmpp:mam:2'));
+    if (jid != null) {
+      x.addField(FieldElement.build(varAttr: 'with', value: jid.userAtDomain));
+    }
+    if (max != null || after != null) {
+      final set = XmppElement('set')
+        ..addAttribute(
+            XmppAttribute('xmlns', 'http://jabber.org/protocol/rsm'));
+      if (max != null) {
+        set.addChild(XmppElement('max')..textValue = max.toString());
+      }
+      if (after != null) {
+        set.addChild(XmppElement('after')..textValue = after);
+      }
+      if (before != null) {
+        set.addChild(XmppElement('before')..textValue = before);
+      }
+      query.addChild(set);
+    }
+    if (flipPage) {
+      query.addChild(
+        XmppElement('flip-page'),
+      );
+    }
+    return _queryMessages(iqStanza, query);
   }
 }
 
@@ -121,4 +232,14 @@ extension MamModuleGetter on Connection {
   MessageArchiveManager getMamModule() {
     return MessageArchiveManager.getInstance(this);
   }
+}
+
+class QueryResult {
+  final bool complete;
+  final int count;
+  final String? first;
+  final String? last;
+  final List<Message> messages;
+
+  QueryResult(this.complete, this.count, this.first, this.last, this.messages);
 }
